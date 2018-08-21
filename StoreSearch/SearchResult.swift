1d081bf84d93023072f9f943b48e7b4dfa7e744a
Alex Fedoseev <lxfedoseev@gmail.com>
2018-08-21 22:22:15 +0300

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Alex Fedoseev on 21.08.2018.
+//  Copyright © 2018 Alex Fedoseev. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
