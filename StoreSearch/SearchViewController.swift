9827e350ce137c7c9d0ad36e652ef2ab8432729a
Alex Fedoseev <lxfedoseev@gmail.com>
2018-08-22 21:56:19 +0300

    style the search bar and add the icon

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -29,6 +29,8 @@ class SearchViewController: UIViewController {
                            forCellReuseIdentifier: TableViewCellIdentifiers.nothingFoundCell)
         
         tableView.rowHeight = 80
+        
+        searchBar.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
